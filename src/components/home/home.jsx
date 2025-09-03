522dfbd8a47e260b7dead25c86e873d9b11c2dc9
Akash <akashkrishnan806@gmail.com>
2025-09-03 11:27:09 +0530

    Third Commit

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -36,7 +36,7 @@ export default function HomePage() {
         >
           <option value="">All Regions</option>
           <option value="Africa">Africa</option>
-          <option value="Americas">Americas</option>
+          <option value="Americas">America</option>
           <option value="Asia">Asia</option>
           <option value="Europe">Europe</option>
           <option value="Oceania">Oceania</option>
