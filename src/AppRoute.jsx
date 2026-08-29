75981c623b1646f4dbf5ebbc582f813096a1471a
Akash <akashkrishnan806@gmail.com>
2026-08-29 16:46:01 +0530

    Fix page import casing

--- src/AppRoute.jsx
+++ src/AppRoute.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import CountryDetails from "./pages/countryDetails";
+import Home from "./pages/Home";
+import CountryDetails from "./pages/CountryDetails";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 import "./App.css";
